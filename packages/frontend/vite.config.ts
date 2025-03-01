f9c2a95b8cbf4149bff914b9e39ad13e3d706904
IsaFxck <jakel2856@gmail.com>
2025-03-02 02:16:50 +0300

    Add PWA support with Vite Plugin PWA
    - Configure PWA manifest with app details and icons
    - Enable auto-update and development mode for PWA
    - Add PWA plugin to Vite configuration
    - Update package dependencies to include vite-plugin-pwa

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +12,46 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'char.fxck',
+        short_name: 'char.fxck',
+        description: 'Создай своего персонажа в char.fxck',
+        theme_color: '#fff',
+        background_color: '#000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
